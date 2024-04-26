f7d9b310058b3a67e4ea606afc5b3edf99a5682f
Alexander Allan <alexallan2392@gmail.com>
2024-04-26 00:18:39 +0000

    updating dims

--- models/ecom_data/marts/dim_inventory_sales.sql
+++ models/ecom_data/marts/dim_inventory_sales.sql
@@ -1,16 +1,18 @@
 SELECT 
-SKU_Code, 
-ROUND(SUM(Amount),2) as Item_sales, 
-inventory.Category, 
-COUNT(Order_ID) as Num_sold, 
-inventory.Size, 
-Color
+    SKU_Code, 
+    ROUND(SUM(Amount),2) as Item_sales, 
+    ROUND(AVG(Amount),2) as Avg_price,
+    inventory.Category, 
+    COUNT(Order_ID) as Num_sold, 
+    inventory.Size, 
+    Color
 FROM {{ ref ('stg_inventory')}} as inventory
 JOIN {{ ref ('stg_amazon_sales')}} as sales 
 ON inventory.SKU_CODE = sales.SKU
-WHERE Amount != 0
-GROUP BY SKU_CODE, 
-inventory.Category, 
-inventory.Size, 
-Color
+WHERE Amount != 0 AND Status != 'Cancelled'
+GROUP BY 
+    SKU_CODE, 
+    inventory.Category, 
+    inventory.Size, 
+    Color
 ORDER BY num_sold DESC
